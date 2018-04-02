9b8c5f7bd5c0cf5b737d54a41bea0c8fd0931b55
Francisco <franciscogd@gatech.edu>
2018-04-02 16:04:03 -0400

    infinite loop with 2s delay

--- build_server/dockerfile.py
+++ build_server/dockerfile.py
@@ -1,27 +1,31 @@
 import mysql.connector
 from utilities import handle_request
 import authentication.credentials as db_credentials
+import time
 
-try:
-    mysql_cnx = mysql.connector.connect(**db_credentials.config)
+while True:
+    try:
+        mysql_cnx = mysql.connector.connect(**db_credentials.config)
 
-    cursor = mysql_cnx.cursor(dictionary=True)
+        cursor = mysql_cnx.cursor(dictionary=True)
 
-    sql_query_get_requests = "SELECT T.template_name, T.os, T.os_version, T.talend_version, " \
-                             "T.talend_component, T.jdk_version, T.jdk_update, T.tomcat_version, " \
-                             "U.region, U.firstname, R.username, R.request_uuid " \
-                             "FROM self_service_db.TEMPLATE T " \
-                             "INNER JOIN self_service_db.REQUEST R ON R.template_uuid = T.template_uuid " \
-                             "INNER JOIN self_service_db.USER U ON R.username = U.username " \
-                             "WHERE R.request_status='pending' "
-except mysql_cnx.Error as e:
-    print(e)
+        sql_query_get_requests = "SELECT T.template_name, T.os, T.os_version, T.talend_version, " \
+                                 "T.talend_component, T.jdk_version, T.jdk_update, T.tomcat_version, " \
+                                 "U.region, U.firstname, R.username, R.request_uuid " \
+                                 "FROM self_service_db.TEMPLATE T " \
+                                 "INNER JOIN self_service_db.REQUEST R ON R.template_uuid = T.template_uuid " \
+                                 "INNER JOIN self_service_db.USER U ON R.username = U.username " \
+                                 "WHERE R.request_status='pending' "
+    except mysql_cnx.Error as e:
+        print(e)
 
-cursor.execute(sql_query_get_requests)
-for request in cursor:
-    print(request)
-    handle_request.handle_request(request)
+    cursor.execute(sql_query_get_requests)
+    for request in cursor:
+        print(request)
+        handle_request.handle_request(request)
 
+    mysql_cnx.close()
+    time.sleep(2)
 
 
 
