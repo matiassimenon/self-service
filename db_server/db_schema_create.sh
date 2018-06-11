cb251597126a48d7a4aebaa50cee8497c398a6e2
Francisco <franciscogd@gatech.edu>
2018-06-11 17:12:20 -0400

    removed db from execution since sql stored procedure does the jon

--- db_server/db_schema_create.sh
+++ db_server/db_schema_create.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-mysql -u root self_service_db -p < /home/talend/self-service/db_server/db_schema_create.sql
+sudo mysql -u root -p < /home/talend/self-service/db_server/db_schema_create.sql
