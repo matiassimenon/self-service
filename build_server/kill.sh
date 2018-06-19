1f9a3f0c46dc1b7b16c243ef68a0ba629043f860
Francisco <franciscogd@gatech.edu>
2018-06-19 15:27:20 -0400

    fixed kill.sh

--- build_server/kill.sh
+++ build_server/kill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=$(ps -aef | grep '/home/centos/self-service/build_server/dockerfile.py' | grep -v grep | awk '{print $2}')
+PID=$(ps -aef | grep dockerfile.py | grep -v grep | awk '{print $2}')
 
 kill -9 "$PID"
 
